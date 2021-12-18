ac358d4c1833e19935f05e1e9e3b46722fb5f110
hazel-shih <hazelshih17@gmail.com>
2021-12-18 22:39:39 +0800

    Updates

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const POST_PER_PAGE = 5;
-export const MEDIA_QUERY_SM = "@media screen and (max-width: 480px)";
-export const MEDIA_QUERY_MD = "@media screen and (max-width: 768px)";
-export const MEDIA_QUERY_LG = "@media screen and (max-width: 1080px)";
--- src/utils.js
+++
@@ -1,4 +0,0 @@
-export function getPreText(body) {
-  if (body.length <= 300) return body;
-  return body.slice(0, 200);
-}
